9361a42aef2ca33fa6c8c2221b2fd90b37034c98
Sajidhgn <sajidhgn@gmail.com>
2023-12-06 03:17:47 +0500

    home data issue fixed

--- src/controllers/about.Controller.ts
+++ src/controllers/about.Controller.ts
@@ -1,22 +1,52 @@
 import {AboutModel} from "../models/about.model";
 import {generateToken} from "../middlewares/auth.helper";
 
-
 // Banner section
 export const Banner = async (req: any, res: any) => {
-
     try {
         const data = await AboutModel.findByIdAndUpdate(req.params.id, { $set: { banner: req.body.payload } });
 
         if (!data) {
-            return  res.json({ "status": "error",message: 'Data not found' });
+            return  res.status(404).json({ "status": "error",message: 'Data not found' });
         }
-        res.json({ "status": "success",message: 'Data updated successfully'});
+        res.status(200).json({ "status": "success",message: 'Data updated successfully'});
     } catch (error) {
 
-        res.json({ "status": "error", message: 'Internal server error' });
+        res.status(500).json({ "status": "error", message: 'Internal server error' });
     }
 };
+
+// Banner Slider
+export const BannerSlider = async (req: any, res: any) => {
+    try {
+        const data = await AboutModel.findByIdAndUpdate(req.params.id, { $set: { banner_slider: req.body.payload } });
+
+        if (!data) {
+            return  res.status(404).json({ "status": "error",message: 'Data not found' });
+        }
+        res.status(200).json({ "status": "success",message: 'Data updated successfully'});
+    } catch (error) {
+
+        res.status(500).json({ "status": "error", message: 'Internal server error' });
+    }
+};
+
+
+// About section
+export const AboutSection = async (req: any, res: any) => {
+    try {
+        const data = await AboutModel.findByIdAndUpdate(req.params.id, { $set: { about_section: req.body.payload } });
+
+        if (!data) {
+            return  res.status(404).json({ "status": "error",message: 'Data not found' });
+        }
+        res.status(200).json({ "status": "success",message: 'Data updated successfully'});
+    } catch (error) {
+
+        res.status(500).json({ "status": "error", message: 'Internal server error' });
+    }
+};
+
 
 // Get List
 export const ServicesData = async (req: any, res: any) => {
@@ -24,11 +54,11 @@ export const ServicesData = async (req: any, res: any) => {
         if(req.body.list){
             const data = await AboutModel.find({});
             if (!data) {
-                return res.json({status: 'error', message: 'Data not found'});
+                return res.status(404).json({status: 'error', message: 'Data not found'});
             }
-            res.json({status: 'success', message: 'Success', data: data});
+            res.status(200).json({status: 'success', message: 'Success', data: data});
         }
     } catch (error) {
-        res.json({status: 'error', message: 'Internal Server Error'});
+        res.status(500).json({status: 'error', message: 'Internal Server Error'});
     }
 };
--- src/routes/home.route.ts
+++ src/routes/home.route.ts
@@ -4,7 +4,7 @@ import {checkUserAuth} from "../middlewares/auth.helper";
 const router = express.Router();
 
 const homeRoute = router
-    .post('/list',checkUserAuth, homeController.homeData)
+    .post('/list', homeController.homeData)
     .put('/banner/:id', checkUserAuth, homeController.Banner)
     .put('/industrypicker/:id',checkUserAuth, homeController.IndustryPicker)
     .put('/services/:id',checkUserAuth, homeController.DevelopmentServices)
